d2f895ba3ad40433dac206dfa271ec4fa1c9d7af
Ryan Munro <monbrey@gmail.com>
2022-12-02 18:04:07 +1100

    fix: comment formatting

--- src/two/main.py
+++ src/two/main.py
@@ -4,13 +4,13 @@ import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
+
+def selection_score(x): return ord(x) - 64
 # Calculate the score for your selection
 
 
-def selection_score(x): return ord(x) - 64
-
-
-def win_score(opp, you):
+def result_score(opp, you):
+  # Calculate the score for the result
   if (opp == you):
     return 3
   match opp:
@@ -22,29 +22,34 @@ def win_score(opp, you):
       return 6 if you == 'A' else 0
 
 
+# Parse turns
 turns = [[line[0], chr(ord(line[2]) - 23)]
          for line in open(f"{dir_path}/input.txt", "r")]
 
-scores = [selection_score(turn[1]) + win_score(turn[0], turn[1])
+# Calculate score per turn
+scores = [selection_score(turn[1]) + result_score(turn[0], turn[1])
           for turn in turns]
 
+# Get total
 print(sum(scores))
+
 
 # Part 2
 
-# Determine what you should select to win, lose or draw
-
 
 def selection(opp, goal):
+  # Determine what you should select to win, lose or draw
   def win(x): return 'B' if x == 'A' else 'C' if x == 'B' else 'A'
   def lose(x): return 'C' if x == 'A' else 'A' if x == 'B' else 'B'
 
   return lose(opp) if goal == 'A' else opp if goal == 'B' else win(opp)
 
 
+# Calculate score per turn using new selection
 scores = [
     selection_score(selection(turn[0], turn[1])) +
-    win_score(turn[0], selection(turn[0], turn[1]))
+    result_score(turn[0], selection(turn[0], turn[1]))
     for turn in turns]
 
+# Get total
 print(sum(scores))
