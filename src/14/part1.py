a7fc9cc619fa539fd5784aefb0de707bb026e2d0
Ryan Munro <monbrey@gmail.com>
2022-12-15 07:28:03 +1100

    feat: day 14

---
+++ src/14/part1.py
@@ -0,0 +1,60 @@
+import os
+import sys
+import numpy as np
+np.set_printoptions(threshold=sys.maxsize)
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+input = [list(map(lambda x: list(map(lambda y: int(y), x.split(','))), line.split(' -> '))) for line in open(f"{dir_path}/input.txt", "r").read().split('\n')]
+
+x1 = min(c[0] for path in input for c in path)
+x2 = max(c[0] for path in input for c in path)
+y = max(c[1] for path in input for c in path)
+
+print(x1,x2,y)
+
+pos = lambda p: [p[1], p[0] - x1]
+wall = np.full((y+1,x2-x1+1), '.')
+
+for path in input:
+	for i in range(0,len(path)-1):
+		start = pos(path[i])
+		end = pos(path[i+1])
+		l,r = sorted([start[0],end[0]])
+		u,d = sorted([start[1],end[1]])
+		for x in range(l,r+1):
+			wall[x,u] = '#'
+		for y in range(u,d+1):
+			wall[l,y] = '#'
+
+overflowing = False
+sand = pos([500,0])
+placed = 0
+
+def fall(grain):
+	global overflowing, placed
+	next = tuple([grain[0]+1, grain[1]])
+	while wall[next] == '.':
+		grain = next
+		next = tuple([grain[0]+1, grain[1]])
+		
+		if next[0] >= wall.shape[0]:
+			return False
+	if next[1]-1 < 0 or next[1]+1 >= wall.shape[1]:
+		return False
+	left,right = tuple([next[0],next[1]-1]), tuple([next[0],next[1]+1])
+	if wall[left] == '.':
+		return fall(left)
+	if wall[right] == '.':
+		return fall(right)
+
+	wall[grain] = 'o'
+	placed += 1
+	return True
+
+while fall(sand):
+	continue
+
+for i, row in enumerate(wall):
+	print(i, ''.join(row))
+
+print(placed)
